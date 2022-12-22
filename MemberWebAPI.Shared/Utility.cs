6e87cc0bb900c200cc3565386798491509d3dcfa
yuguangjiang <yuguangjiang@liontravel.com>
2022-12-22 15:23:30 +0800

    拉出共用方法至Shard、建立DataAccess

---
+++ MemberWebAPI.Shared/Utility.cs
@@ -0,0 +1,98 @@
+﻿using MemberWebAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberWebAPI.Shared
+{
+    internal class Utility
+    {
+        /// <summary>
+        /// 密碼加密
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private string PasswordHash(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        /// <summary>
+        /// 驗證密碼
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="dbPassword"></param>
+        /// <returns></returns>
+        private bool ValidatePasswordHash(string password, string dbPassword)
+        {
+            byte[] hashBytes = Convert.FromBase64String(dbPassword);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 產生Access Token
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        private string GetJWTAuthKey(Account account, List<Group> groups, TokenSettings tokenSettings)
+        {
+            var securtityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Key));
+
+            var credentials = new SigningCredentials(securtityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim("Email", account.Account1));
+            claims.Add(new Claim("UserName", account.UserName));
+            if ((groups?.Count ?? 0) > 0)
+            {
+                foreach (var group in groups)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, group.GroupName));
+                }
+            }
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: tokenSettings.Issuer,
+                audience: tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials,
+                claims: claims
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        }
+    }
+}
---
+++ MemberWebAPI/DataAccess/AuthLoginDataAccessLayer.cs
@@ -0,0 +1,21 @@
+﻿using MemberWebAPI.DbContexts;
+using MemberWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MemberWebAPI.DataAccess
+{
+    public class AuthLoginDataAccessLayer
+    {
+        readonly EldPlatContext _eldPlatContext;
+        readonly TokenSettings _tokenSettings;
+        readonly IHttpContextAccessor _httpcontextAccessor;
+
+        public AuthLoginDataAccessLayer(IDbContextFactory<EldPlatContext> eldPlatContext, IOptions<TokenSettings> tokenSettings, IHttpContextAccessor httpcontextAccessor)
+        {
+            _eldPlatContext = eldPlatContext.CreateDbContext();
+            _tokenSettings = tokenSettings.Value;
+            _httpcontextAccessor = httpcontextAccessor;
+        }
+    }
+}
